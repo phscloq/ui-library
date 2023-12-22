db48c1b9a2959ac886de760366548dcc5c0182a1
Baran Dogan <phscloq@gmail.com>
2023-12-22 12:49:17 +0300

    Delete Left-Nav and Navbar components

--- src/app/components/Left-Nav.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link"
-export default function LeftNavbar(){
-    return (
-        <div className=" pl-8 left-0  top-0 bg-slate-400 w-1/6 h-screen">
-            <Link href="/components"><button>Components</button></Link>
-        </div>
-    )
-}

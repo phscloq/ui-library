d5ae08ed7ef8ef2e31d2acaf2ab42a92b90077e1
Baran Dogan <phscloq@gmail.com>
2023-12-22 13:57:05 +0300

    Add content

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,7 +1,19 @@
 export default function Docs(){
     return (
-        <div>
-            <h1>Introduction</h1>
+        <div className=" w-2/3 p-8  font-light  leading-8">
+            <h1 className=" text-3xl">Introduction</h1>
+            <h3 className=" font-normal">What's this site for?</h3>
+            <div className="flex flex-col gap-4">
+            <p>Baran UI is kinda ui library. "Kinda" because it is not a package.</p>
+               <p>Over the course of my journey at web development I felt the need for ready-to-use components.
+               And as a learning experience I have decided to built mine. This will help me to code from zero every web app that I am building.
+            </p>
+            <p>It is free to use. No citetation necessary.
+                If you think there is a better way to code better the components you see here, you can edit on github.
+                Only obligation is using tailwindcss. Thank you for your time.
+            </p>
+            </div>
+            
         </div>
     )
 }

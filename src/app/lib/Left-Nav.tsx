17f6fd868bf406d724e92fd10cfb07f623c9486f
Baran Dogan <phscloq@gmail.com>
2023-12-24 20:53:14 +0300

    Add LeftNav Links's pathname styling

--- src/app/lib/Left-Nav.tsx
+++ src/app/lib/Left-Nav.tsx
@@ -1,5 +1,8 @@
+'use client'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 export default function LeftNavbar(){
+    const pathname = usePathname();
     return (
         <div className="left-0  border-r top-0 h-screen  w-1/5 flex-shrink-0">
             <div className="sticky top-0 left-0 z-10">
@@ -13,14 +16,14 @@ export default function LeftNavbar(){
             </div>
             <div className="absolute p-8 flex flex-col text-gray-400 overflow-y-auto gap-4">
                 <h4 className="text-black">Getting Started</h4>
-                <Link className="" href="/docs">
+                <Link className={`${pathname === "/docs" ? 'text-black' : '' }`} href="/docs">
                     <button>Introduction</button>
                 </Link>
                 <h4  className="text-black">Components</h4>
-                <Link className="" href="/docs/components/accordion">
+                <Link className={`${pathname === "/docs/components/accordion" ? 'text-black' : '' }`} href="/docs/components/accordion">
                     <button>Accordion</button>
                 </Link>
-                <Link className="" href="/docs/components/calendar">
+                <Link className={`${pathname === "/docs/components/calendar" ? 'text-black' : '' }`} href="/docs/components/calendar">
                     <button>Calendar</button>
                 </Link>
            

f7f68380e3e72055a1a4a446cdb08997dbd3f618
Baran Dogan <phscloq@gmail.com>
2023-12-24 20:52:24 +0300

    Add accordion components

---
+++ src/app/docs/components/accordion/accordion.tsx
@@ -0,0 +1,106 @@
+'use client'
+import { useState } from "react"
+export  function AccordionComponent(){
+    const [accordion, setAccordion] = useState(false);
+
+return (
+    <div className="">
+                <button className="w-full flex justify-between" onClick={()=>{setAccordion(!accordion)}}>
+                    <span>Section</span>
+                    {accordion ? <span>-</span>:<span>+</span>}
+                </button>
+                <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-sm ${
+          accordion
+            ? "grid-rows-[1fr] opacity-100"
+            : "grid-rows-[0fr] opacity-0"
+        }`}
+      >
+        <p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
+      </div>
+        </div>
+)
+}
+export function AccordionArrow(){
+    const [accordion, setAccordion] = useState(false);
+
+    return (
+        <div className="">
+                    <button className="w-full flex justify-between" onClick={()=>{setAccordion(!accordion)}}>
+                        <span>Section</span>
+                        {accordion ? <span className=" rotate-90">{'>'}</span>:<span className="rotate-90">{'<'}</span>}
+                    </button>
+                    <div
+            className={`grid overflow-hidden transition-all duration-300 ease-in-out text-sm ${
+              accordion
+                ? "grid-rows-[1fr] opacity-100"
+                : "grid-rows-[0fr] opacity-0"
+            }`}
+          >
+            <p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
+          </div>
+            </div>
+    ) 
+}
+
+export function AccordionTriple(){
+    const [accordionArr, setAccordionArr] = useState([false, false , false]);
+    const toggleAccordion = (index:number) => {
+        const newAccordionArr = [...accordionArr];
+        newAccordionArr[index] = !newAccordionArr[index];
+        setAccordionArr(newAccordionArr);
+    };
+    return(
+        <div>
+              <div className=" mb-2 ">
+        <button className="w-full flex  justify-between" 
+        onClick={()=>{toggleAccordion(0)}}>
+            <span>Section 1</span>
+            {accordionArr[0] ? <span className=" rotate-90">{'>'}</span>:<span className="rotate-90">{'<'}</span>}
+        </button>
+        <div
+className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600  text-sm ${
+  accordionArr[0]
+    ? "grid-rows-[1fr] opacity-100"
+    : "grid-rows-[0fr] opacity-0"
+}`}
+>
+<p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
+</div>
+              </div>
+              <div className=" mb-2 " >
+        <button className="w-full flex  justify-between" 
+        onClick={()=>{toggleAccordion(1)}}>
+            <span>Section 2</span>
+            {accordionArr[1] ? <span className=" rotate-90">{'>'}</span>:<span className="rotate-90">{'<'}</span>}
+        </button>
+        <div
+className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600  text-sm ${
+  accordionArr[1]
+    ? "grid-rows-[1fr] opacity-100"
+    : "grid-rows-[0fr] opacity-0"
+}`}
+>
+<p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
+</div>
+              </div>
+              <div className="mb-2 " >
+        <button className="w-full flex  justify-between" 
+        onClick={()=>{toggleAccordion(2)}}>
+            <span>Section 3</span>
+            {accordionArr[2] ? <span className=" rotate-90">{'>'}</span>:<span className="rotate-90">{'<'}</span>}
+        </button>
+        <div
+className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+  accordionArr[2]
+    ? "grid-rows-[1fr] opacity-100"
+    : "grid-rows-[0fr] opacity-0"
+}`}
+>
+<p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
+</div>
+              </div>
+        </div>
+      
+    )
+}
--- src/app/docs/components/accordion/page.tsx
+++ src/app/docs/components/accordion/page.tsx
@@ -1,7 +1,32 @@
+import { AccordionComponent, AccordionArrow, AccordionTriple } from "./accordion";
 export default function Accordion(){
     return (
-        <div>
-            <h1>Accordion</h1>
+        <div className="p-8  w-2/3">
+            <div className="flex mb-4">   
+            <p className=" text-gray-400">docs{'>'}components{'>'}</p>
+                      <p className="text-black">accordion</p>
+            </div>
+                    
+            <h1 className="text-2xl mb-8">Accordion</h1>
+            <div className="mb-8">
+                <h4 className="mb-4">This is a single accordion component:</h4>
+                <div className=" p-4 bg-gray-200 rounded-lg">
+                    <AccordionComponent />
+                </div>
+            </div>
+           <div className="mb-8">
+            <h4 className="mb-4">Let's do again but this time with an arrow:</h4>
+                <div className=" p-4 bg-gray-200 rounded-lg">
+                    <AccordionArrow />
+                </div>
+           </div>
+           <div className="">
+            <h4 className="mb-4">And with three sections:</h4>
+                <div className=" p-4 bg-gray-200 rounded-lg">
+                    <AccordionTriple />
+                </div>
+           </div>
+          
         </div>
     )
 }

92b385b138cbd3946756341b53fd7668eef82797
Baran Dogan <phscloq@gmail.com>
2024-01-02 23:59:14 +0300

    Add dark mode and display current pathname in Calendar component

--- src/app/docs/components/calendar/page.tsx
+++ src/app/docs/components/calendar/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+import { useThemeContext } from "@/app/context";
+import { usePathname } from "next/navigation"
+
 export default function Calendar(){
+    const {dark} = useThemeContext();
+    const pathname = usePathname();
     return (
-        <div>
+
+        <div className={` ${dark ? 'bg-slate-600': ''}`}>
+            <p className="text-white">{pathname}</p>
             <h1>Calendar</h1>
+            <p className={`${dark ? ' text-red-500': 'text-blue-500'}`}>Tema</p>
             <div className=" bg-red-600 h-96"></div>
             <div className=" bg-blue-400 h-96"></div>
             <div className=" bg-green-600 h-96"></div>

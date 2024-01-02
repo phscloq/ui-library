8a173d1355cf5f1b7b2bc1e0c0339aea266832da
Baran Dogan <phscloq@gmail.com>
2024-01-02 23:57:30 +0300

    Add dark mode support and update LeftNavbar styling

--- src/app/lib/Left-Nav.tsx
+++ src/app/lib/Left-Nav.tsx
@@ -1,10 +1,14 @@
 'use client'
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useContext } from "react";
+import { ThemeContext, useThemeContext } from "../context";
 export default function LeftNavbar(){
+const {dark} = useThemeContext();
     const pathname = usePathname();
     return (
-        <div className="left-0  border-r top-0 h-screen  w-1/5 flex-shrink-0">
+        <div className={`${dark ? 'bg-slate-600 border-slate-700 ': ''}
+        left-0  border-r top-0 h-screen sticky  w-1/6 flex-shrink-0 `}>
             <div className="sticky top-0 left-0 z-10">
                 <div className="pt-8">
                     <Link 
@@ -23,8 +27,11 @@ export default function LeftNavbar(){
                 <Link className={`${pathname === "/docs/components/accordion" ? 'text-black' : '' }`} href="/docs/components/accordion">
                     <button>Accordion</button>
                 </Link>
-                <Link className={`${pathname === "/docs/components/calendar" ? 'text-black' : '' }`} href="/docs/components/calendar">
+                {/* <Link className={`${pathname === "/docs/components/calendar" ? 'text-black' : '' }`} href="/docs/components/calendar">
                     <button>Calendar</button>
+                </Link> */}
+                <Link className={`${pathname === "/docs/components/carousel" ? 'text-black' : '' }`} href="/docs/components/carousel">
+                    <button>Carousel</button>
                 </Link>
            
      

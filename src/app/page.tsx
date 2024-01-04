be040c443dbda3c52744257598585460f78a88ad
Baran Dogan <phscloq@gmail.com>
2024-01-04 14:23:50 +0300

    Refactor main className in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 
 export default function Home() {
+  
   return (
-    <main className="p-8  w-2/3"> 
+    <main className=""> 
     <h1 className=" text-3xl">Welcome to Baran UI</h1> 
  </main>
   )

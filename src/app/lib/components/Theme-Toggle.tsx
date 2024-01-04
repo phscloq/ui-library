e8e2f76c40814abd4a64e9191a86d28ba0d49358
Baran Dogan <phscloq@gmail.com>
2024-01-04 14:13:52 +0300

    Refactor ThemeToggle component to use useContext hook

--- src/app/lib/components/Theme-Toggle.tsx
+++ src/app/lib/components/Theme-Toggle.tsx
@@ -1,14 +1,8 @@
 "use client"
-
-import { useEffect, useState } from "react";
-import { create, getThemeCookie } from "@/app/utils/cookies";
+import { useContext } from "react";
+import { ThemeContext } from "@/app/Theme-Context";
 export default function ThemeToggle(){
-    const [theme, setTheme] = useState<string>("");
-
-    const handleThemeChange = (theme:string) => {
-      setTheme(theme);
-      create(theme);
-    };
+    const {theme, handleThemeChange} = useContext(ThemeContext);
     
     return (
         <button onClick={()=>handleThemeChange(theme==='light' ? 'dark' : 'light')}>Lights</button>

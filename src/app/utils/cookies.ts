dd48871f3c7601f56d80103e997f3a4e446b9c47
Baran Dogan <phscloq@gmail.com>
2024-01-04 14:16:40 +0300

    Fix theme cookie creation and retrieval

--- src/app/utils/cookies.ts
+++ src/app/utils/cookies.ts
@@ -4,7 +4,6 @@ import { cookies } from 'next/headers'
  
 export async function create(theme:string) {
   cookies().set('theme', theme)
-  // or
 }
 export const getThemeCookie = async () => {
     return cookies().get('theme');

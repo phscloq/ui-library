7bec7724aaa59996ba00899b1cc65246ff1da2cf
Baran Dogan <phscloq@gmail.com>
2024-01-09 12:26:09 +0300

    Add Accordion Code Preview component

--- src/app/docs/components/accordion/components/CodePreview.tsx
+++ src/app/docs/components/accordion/components/CodePreview.tsx
@@ -1,49 +1,5 @@
-'use client'
-import { useState } from "react"
-export  function AccordionComponent(){
-    const [accordion, setAccordion] = useState(false);
-
-return (
-    <div className=" text-black">
-                <button className="w-full flex justify-between" onClick={()=>{setAccordion(!accordion)}}>
-                    <span>Section</span>
-                    {accordion ? <span>-</span>:<span>+</span>}
-                </button>
-                <div
-        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-sm ${
-          accordion
-            ? "grid-rows-[1fr] opacity-100"
-            : "grid-rows-[0fr] opacity-0"
-        }`}
-      >
-        <p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
-      </div>
-        </div>
-)
-}
-export function AccordionArrow(){
-    const [accordion, setAccordion] = useState(false);
-
-    return (
-        <div className="text-black">
-                    <button className="w-full flex justify-between" onClick={()=>{setAccordion(!accordion)}}>
-                        <span>Section</span>
-                        {accordion ? <span className=" rotate-90">{'>'}</span>:<span className="rotate-90">{'<'}</span>}
-                    </button>
-                    <div
-            className={`grid overflow-hidden transition-all duration-300 ease-in-out text-sm ${
-              accordion
-                ? "grid-rows-[1fr] opacity-100"
-                : "grid-rows-[0fr] opacity-0"
-            }`}
-          >
-            <p className="overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid eos dolorem voluptates maiores?</p>
-          </div>
-            </div>
-    ) 
-}
-
-export function AccordionTriple(){
+export default function CodePreview() {
+    const code = `
     const [accordionArr, setAccordionArr] = useState([false, false , false]);
     const toggleAccordion = (index:number) => {
         const newAccordionArr = [...accordionArr];
@@ -62,12 +18,13 @@ export function AccordionTriple(){
                     <span className="rotate-90">{'<'}</span>}
                 </button>
                 <div
-                className={`grid overflow-hidden transition-all 
-                duration-300 ease-in-out text-slate-600  text-sm ${
-                accordionArr[0]
-                  ? "grid-rows-[1fr] opacity-100"
-                  : "grid-rows-[0fr] opacity-0"
-                }`}
+                className={grid overflow-hidden transition-all 
+                duration-300 ease-in-out text-slate-600  text-sm
+                INSERT LOGIC HERE!
+                accordionArr[0] 
+                When TRUE: grid-rows-[1fr] opacity-100
+                When FALSE: grid-rows-[0fr] opacity-0
+                }
                 >
                     <p className="overflow-hidden">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -89,12 +46,13 @@ export function AccordionTriple(){
                     <span className="rotate-90">{'<'}</span>}
                 </button>
                 <div
-                className={`grid overflow-hidden transition-all duration-300 ease-in-out 
-                text-slate-600  text-sm ${
-                accordionArr[1]
-                  ? "grid-rows-[1fr] opacity-100"
-                  : "grid-rows-[0fr] opacity-0"
-              }`}
+                className={grid overflow-hidden transition-all duration-300 ease-in-out 
+                text-slate-600  text-sm
+                INSERT LOGIC HERE!
+                accordionArr[1] 
+                When TRUE: grid-rows-[1fr] opacity-100
+                When FALSE: grid-rows-[0fr] opacity-0
+                }
               >
                     <p className="overflow-hidden">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -116,12 +74,12 @@ export function AccordionTriple(){
                       <span className="rotate-90">{'<'}</span>}
                   </button>
                   <div
-                  className={`grid overflow-hidden transition-all duration-300 ease-in-out 
-                  text-slate-600 text-sm ${
-                  accordionArr[2]
-                    ? "grid-rows-[1fr] opacity-100"
-                    : "grid-rows-[0fr] opacity-0"
-                  }`}
+                  className={grid overflow-hidden transition-all duration-300 ease-in-out 
+                  text-slate-600 text-sm 
+                  INSERT LOGIC HERE!
+                  accordionArr[2] 
+                  When TRUE: grid-rows-[1fr] opacity-100
+                  When FALSE: grid-rows-[0fr] opacity-0}
                   >
                       <p className="overflow-hidden">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -133,5 +91,17 @@ export function AccordionTriple(){
               </div>
         </div>
     </div>
-    )
-}
+    `;
+    
+    return (
+      <div className="w-full bg-slate-400 h-[400px] p-4
+      rounded-md overflow-auto relative
+     ">
+       <pre className=""><code className="text-black">{code}</code></pre>
+       <button
+       className="bg-slate-300 text-black px-2 py-1 
+       rounded-md absolute top-2 right-9 " 
+       onClick={() => navigator.clipboard.writeText(code)}>Copy</button> 
+      </div>
+    );
+  }
--- src/app/docs/components/accordion/page.tsx
+++ src/app/docs/components/accordion/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import Pathway from "@/app/lib/components/Pathway";
 import { AccordionComponent, AccordionArrow, AccordionTriple } from "./accordion";
+import { useState } from "react";
+import CodePreview from "./components/CodePreview";
 export default function Accordion(){
-
+const [preview, setPreview] = useState(false)
     return (
         <div className={``}>
             
@@ -24,12 +26,19 @@ export default function Accordion(){
                 </div>
            </div>
            <div className="mb-8">
-            <h4 className="mb-4">And with three sections:</h4>
-                <div className=" p-4 bg-gray-200 rounded-lg">
-                    <AccordionTriple />
+            <h4 className="mb-4">Example:</h4>
+                <div className="flex gap-4 text-slate-300 border-b border-slate-400 mb-2">
+                    <button 
+                    className={`${preview ? ' text-slate-200 border-b-2 border-slate-300' : ' text-slate-400'} px-2 py-1 `}
+                    onClick={()=>setPreview(true)}>Preview</button>
+                    <button 
+                    className={`${!preview ? ' text-slate-200 border-b-2 border-slate-300' : ' text-slate-400'} px-2 py-1 `}
+                    onClick={()=>setPreview(false)}>Code</button>
+                </div>
+                
+                  {preview ? <AccordionTriple /> :  <CodePreview /> }  
                 </div>
            </div>
            
-        </div>
     )
 }

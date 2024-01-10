253d20b24f58737a2677435fc269b0109a199d89
Baran Dogan <phscloq@gmail.com>
2024-01-10 23:59:03 +0300

    Refactor layout component and fix imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,10 @@ import { poppins } from './ui/fonts';
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import Navbar from './lib/Navbar';
-import LeftNavbar from './lib/Left-Nav';
+
 import { cookies } from 'next/headers';
 import { ThemeContextProvider, MobileMenuContextProvider } from './contexts';
-import Main from './lib/Main';
+import Main from './lib/components/Main';
 export const metadata: Metadata = {
   title: 'Baran UI',
   description: 'Baran UI Library',
@@ -18,17 +17,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-const cookieStore = cookies();
-const theme = cookieStore.get('theme')?.value ?? 'light';
+
   return (
     <html lang="en">        
       <body 
-      className={`${poppins.className} 
-      ${theme==='light' ? ' bg-white' : 'bg-slate-700 text-stone-100'}
+      className={`${poppins.className}
       flex`}>
-        <ThemeContextProvider
-        value={theme}
-        >
+        <ThemeContextProvider>
           <MobileMenuContextProvider>
             
           <Main>

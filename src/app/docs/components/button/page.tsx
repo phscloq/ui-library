0b3d6e725e1858c25efaef79edc6326de6d04441
Baran Dogan <phscloq@gmail.com>
2024-01-21 22:50:30 +0300

    Remove unused import and add missing import

--- src/app/docs/components/button/page.tsx
+++ src/app/docs/components/button/page.tsx
@@ -1,9 +1,9 @@
 'use client'
-import Pathway from "@/app/lib/components/pathway"
 import Button from "./components/button"
 import CodePreview from "./components/code-preview"
 import { useState } from "react"
 import TabSwitch from "@/app/lib/components/tab-switch"
+import Pathway from "@/app/lib/components/pathway"
 
 export default function Carousel(){
 const [preview, setPreview] = useState(true)

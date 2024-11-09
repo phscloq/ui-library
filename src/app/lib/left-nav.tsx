d7cb10883604c35971fab442c72e0d2eacf8cfc4
Baran Dogan <phscloq@gmail.com>
2024-11-09 23:42:25 +0300

    Update LeftNavbar component to dynamically generate component links

--- src/app/lib/left-nav.tsx
+++ src/app/lib/left-nav.tsx
@@ -5,32 +5,41 @@ import { use, useEffect } from "react";
 
 export default function LeftNavbar({theme}:{theme:string}){
 
+    const components = [
+        {
+            title: 'Accordion',
+            href: '/docs/components/accordion'
+        },
+        {
+            title: 'Button',
+            href: '/docs/components/button'
+        },
+        {
+            title: 'Switch',
+            href: '/docs/components/switch'
+        },
+        {
+            title: 'Form',
+            href: '/docs/components/form'
+        }
+    ]
+
     useEffect(()=>{
         console.log("Left Navbar rerendered")
         console.log(theme)
     })
     const pathname = usePathname();
     return (
-        <div className={`${theme === 'light' ? '':'border-slate-800'}
-         hidden lg:block
+        <aside className={`${theme === 'light' ? '':'border-slate-800'}
+         hidden lg:block py-24 px-8
         left-0  border-r top-0 h-screen sticky  sm:w-1/6 flex-shrink-0 `}>
            
-            <div className="sticky top-0 left-0 z-10 ">{/*Logo div */}
-                <div className="pt-8">
-                    <Link 
-                    className={`text-xl  tracking-wide text-black
-                    ${theme === 'light'? '':'text-stone-100'}
-                    `} href="/">
-                        <h1 className="px-10" >Baran UI</h1>
-                    </Link>
-                </div>
-                
-            </div>
+           
             
             {/* Left Side Navbar List */}
             <div 
             className={`
-            absolute p-8 flex flex-col text-gray-400
+            absolute flex flex-col text-gray-400
             overflow-y-auto gap-4
             ${theme === 'light' ? '':'text-stone-300'}
             `}>
@@ -56,74 +65,22 @@ export default function LeftNavbar({theme}:{theme:string}){
                     `}>
                         Components
                 </h4>
-                
-                <Link 
-                    className={`${pathname === "/docs/components/accordion"
-                    &&
-                    theme === 'light'
-                    ? 'text-black' : '' }
-                    ${pathname === "/docs/components/accordion"
-                    && 
-                    theme === 'dark'  ? 'text-sky-500' : ''}
-                    `} 
-                    href="/docs/components/accordion">
-                    <button>
-                        Accordion
-                    </button>
-                </Link>
-
-                <Link  
-                    className={`${pathname === "/docs/components/button" ? 'text-black' : '' }
-                    ${pathname === "/docs/components/button"
-                    && 
-                    (theme === 'dark' || theme === 'system') ? 'text-sky-500' : ''}
-                    `} 
-                    href="/docs/components/button">
-                    <button>
-                        Button
-                    </button>
-                </Link>
-                {/* <Link className={`${pathname === "/docs/components/calendar" ? 'text-black' : '' }`} href="/docs/components/calendar">
-                    <button>Calendar</button>
-                </Link> */}
-                {/* <Link  
-                    className={`${pathname === "/docs/components/carousel" ? 'text-black' : '' }
-                    ${pathname === "/docs/components/carousel"
-                    && 
-                    (theme === 'dark' || theme === 'system') ? 'text-sky-500' : ''}
-                    `} 
-                    href="/docs/components/carousel">
-                    <button>
-                        Carousel
-                    </button>
-                </Link> */}
-
-                <Link  
-                    className={`${pathname === "/docs/components/switch" ? 'text-black' : '' }
-                    ${pathname === "/docs/components/switch"
-                    && 
-                    (theme === 'dark' || theme === 'system') ? 'text-sky-500' : ''}
-                    `} 
-                    href="/docs/components/switch">
-                    <button>
-                        Switch
-                    </button>
-                </Link>
-                <Link  
-                    className={`${pathname === "/docs/components/form" ? 'text-black' : '' }
-                    ${pathname === "/docs/components/form"
-                    && 
-                    (theme === 'dark' || theme === 'system') ? 'text-sky-500' : ''}
-                    `} 
-                    href="/docs/components/form">
-                    <button>
-                        Form
-                    </button>
-                </Link>
-     
-        
+                {components.map((component, index)=>{
+                    return(
+                        <Link
+                            key={index} 
+                            className={`${pathname === component.href 
+                            && 'text-black dark:text-blue-500' }
+                            `} 
+                            href={component.href}>
+                            <button>
+                                {component.title}
+                            </button>
+                        </Link>
+                    )
+                })}
             </div>
            
-        </div>
+        </aside>
     )
 }

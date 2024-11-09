e22b799c15fc6e82538ad6981fd0e8cc9b7b5e38
Baran Dogan <phscloq@gmail.com>
2024-11-09 23:45:09 +0300

    Update Accordion component to include CodePreview with dynamic code snippet

--- src/app/docs/components/accordion/page.tsx
+++ src/app/docs/components/accordion/page.tsx
@@ -2,9 +2,102 @@
 import Pathway from "@/app/lib/components/pathway";
 import { AccordionComponent, AccordionArrow, AccordionTriple } from "./accordion";
 import { useState } from "react";
-import CodePreview from "./components/code-preview";
 import TabSwitch from "@/app/lib/components/tab-switch";
+import CodePreview from "@/app/lib/components/code-preview";
 export default function Accordion(){
+    const code = `
+    const [accordionArr, setAccordionArr] = useState([false, false , false]);
+    const toggleAccordion = (index:number) => {
+        const newAccordionArr = [...accordionArr];
+        newAccordionArr[index] = !newAccordionArr[index];
+        setAccordionArr(newAccordionArr);
+    };
+    return(
+    <div className=" p-4 bg-gray-200 rounded-lg">
+        <div className="text-black">
+            <div className=" mb-2 ">
+                <button className="w-full flex  justify-between" 
+                onClick={()=>{toggleAccordion(0)}}>
+                    <span>Section 1</span>
+                    {accordionArr[0] ? <span className=" rotate-90">{'>'}</span>
+                    :
+                    <span className="rotate-90">{'<'}</span>}
+                </button>
+                <div
+                className={grid overflow-hidden transition-all 
+                duration-300 ease-in-out text-slate-600  text-sm
+                INSERT LOGIC HERE!
+                accordionArr[0] 
+                When TRUE: grid-rows-[1fr] opacity-100
+                When FALSE: grid-rows-[0fr] opacity-0
+                }
+                >
+                    <p className="overflow-hidden">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Officiis ad quasi eius cupiditate assumenda eaque numquam deleniti. 
+                    Iusto autem quidem obcaecati alias dolor animi, eligendi aliquid 
+                    eos dolorem voluptates maiores?
+                    </p>
+                </div>
+            </div>
+
+
+            <div className=" mb-2 " >
+                <button className="w-full flex  justify-between" 
+                onClick={()=>{toggleAccordion(1)}}>
+                    <span>Section 2</span>
+                    {accordionArr[1] ? 
+                    <span className=" rotate-90">{'>'}</span>
+                    :
+                    <span className="rotate-90">{'<'}</span>}
+                </button>
+                <div
+                className={grid overflow-hidden transition-all duration-300 ease-in-out 
+                text-slate-600  text-sm
+                //INSERT LOGIC HERE!
+                accordionArr[1] 
+                When TRUE: grid-rows-[1fr] opacity-100
+                When FALSE: grid-rows-[0fr] opacity-0
+                }
+              >
+                    <p className="overflow-hidden">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Officiis ad quasi eius cupiditate assumenda eaque numquam 
+                      deleniti. Iusto autem quidem obcaecati alias dolor animi, 
+                      eligendi aliquid eos dolorem voluptates maiores?
+                    </p>
+                </div>
+            </div>
+
+
+              <div className="mb-2 " >
+                  <button className="w-full flex  justify-between" 
+                  onClick={()=>{toggleAccordion(2)}}>
+                      <span>Section 3</span>
+                      {accordionArr[2] ? 
+                      <span className=" rotate-90">{'>'}</span>
+                      :
+                      <span className="rotate-90">{'<'}</span>}
+                  </button>
+                  <div
+                  className={grid overflow-hidden transition-all duration-300 ease-in-out 
+                  text-slate-600 text-sm 
+                  INSERT LOGIC HERE!
+                  accordionArr[2] 
+                  When TRUE: grid-rows-[1fr] opacity-100
+                  When FALSE: grid-rows-[0fr] opacity-0}
+                  >
+                      <p className="overflow-hidden">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Officiis ad quasi eius cupiditate assumenda eaque numquam 
+                        deleniti. Iusto autem quidem obcaecati alias dolor animi, 
+                        eligendi aliquid eos dolorem voluptates maiores?
+                      </p>
+                  </div>
+              </div>
+        </div>
+    </div>
+    `;
 const [preview, setPreview] = useState(true)
     return (
         <div className={``}>
@@ -30,7 +123,7 @@ const [preview, setPreview] = useState(true)
             <h4 className="mb-4">Example:</h4>
                 <TabSwitch preview={preview} setPreview={setPreview} />
                 
-                  {preview ? <AccordionTriple /> :  <CodePreview /> }  
+                  {preview ? <AccordionTriple /> :  <CodePreview>{code}</CodePreview> }  
                 </div>
            </div>
            

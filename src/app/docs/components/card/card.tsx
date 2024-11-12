2de0491ead30b9dbf90de6d141e44b509072d52d
Baran <phscloq@gmail.com>
2024-11-12 23:19:55 +0300

    Update Card component styling for dark mode

--- src/app/docs/components/card/card.tsx
+++ src/app/docs/components/card/card.tsx
@@ -6,10 +6,10 @@ export default function CardComponent(){
         <div className="bg-white max-w-md shadow-md dark:bg-slate-600 border dark:border-white rounded-md ">
             <div className="p-6">
                 <h3 className="text-xl font-semibold mb-2
-                 text-gray-800 dark:text-black">
+                 text-gray-800 dark:text-stone-100">
                     Title
                 </h3>
-                <p className=" text-gray-600 dark:text-slate-300">
+                <p className=" text-gray-600 dark:text-stone-300">
                     This is an example of card. You can use this card to display information in a structured way.
                     By changing the width of the card div, you can adjust the size of the card.
                 </p>

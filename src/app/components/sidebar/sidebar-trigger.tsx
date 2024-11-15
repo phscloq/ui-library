bc3005551d044149f124320d70ad21800e518445
Baran <phscloq@gmail.com>
2024-11-15 22:45:04 +0300

    Update SidebarTrigger component to adjust left navigation styling

--- src/app/components/sidebar/sidebar-trigger.tsx
+++ src/app/components/sidebar/sidebar-trigger.tsx
@@ -8,7 +8,7 @@ export default function SidebarTrigger(){
 
     return (
         <button className={`lg:hidden block text-black bg-slate-200 dark:bg-slate-600 p-2 rounded-r-full fixed top-1/2 z-30
-            ${sideBarOpen ? 'right-10  dark:bg-slate-700' : 'left-0'}
+            ${sideBarOpen ? '  left-[248px] dark:bg-slate-700' : 'left-0'}
             `}
             onClick={()=>handleLeftNavChange(!sideBarOpen)}
             >

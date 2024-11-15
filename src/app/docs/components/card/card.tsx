e931dcf52353443e2f2c380d197333d0c6278516
Baran <phscloq@gmail.com>
2024-11-15 22:46:42 +0300

    Update CardComponent styling for small screens

--- src/app/docs/components/card/card.tsx
+++ src/app/docs/components/card/card.tsx
@@ -1,15 +1,15 @@
 export default function CardComponent(){
     return(
-    <div className="w-full bg-slate-400 h-[400px] flex gap-2 justify-center items-center
+    <div className="w-full bg-slate-400 h-[400px] flex gap-2 justify-center items-center p-4 sm:p-0
         rounded-md
        ">
-        <div className="bg-white max-w-md shadow-md dark:bg-slate-600 border dark:border-white rounded-md ">
+        <div className="bg-white max-w-md shadow-md dark:bg-slate-600 border dark:border-white rounded-md max-h-72 ">
             <div className="p-6">
                 <h3 className="text-xl font-semibold mb-2
                  text-gray-800 dark:text-stone-100">
                     Title
                 </h3>
-                <p className=" text-gray-600 dark:text-stone-300">
+                <p className=" text-gray-600 dark:text-stone-300  max-h-40 overflow-scroll ">
                     This is an example of card. You can use this card to display information in a structured way.
                     By changing the width of the card div, you can adjust the size of the card.
                 </p>

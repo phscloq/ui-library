99e881ae27469e4cbbf3b0af8417bcc295f8110a
Baran <phscloq@gmail.com>
2024-12-05 12:08:51 +0300

    Update tab-switch-tsx
    Refactor TabSwitch component to style tab switching functionality.

--- src/app/components/tab-switch.tsx
+++ src/app/components/tab-switch.tsx
@@ -1,27 +1,52 @@
 'use client'
-import { ThemeContext } from "@/app/contexts";
-import { useContext } from "react";
-
+import { useEffect, useRef, useState } from "react";
 interface TabSwitchProps{
     preview: boolean,
     setPreview: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export default function TabSwitch({preview, setPreview}:TabSwitchProps){
-const {theme} = useContext(ThemeContext);
+    const [backgroundStyle, setbackgroundStyle] = useState({
+        transform: `translateX(8px)`,
+        width: 76,
+        opacity: 1
+     });
+    const tabRefs = useRef<(HTMLButtonElement | null)[]>([]);
+
+    useEffect(()=>{
+        const activeTabElement = tabRefs.current[preview ? 0 : 1];
+        if(activeTabElement){
+            const {offsetLeft, offsetWidth} = activeTabElement;
+            setbackgroundStyle({
+                transform: `translateX(${offsetLeft}px)`,
+                width: offsetWidth,
+                opacity: 1
+            })
+        }
+    },[preview])
+
     return(
-    <div className="flex gap-4 text-slate-300 border-b border-slate-400 mb-2">
+    <div className="flex gap-4 text-slate-300 bg-slate-600   rounded-t-md relative ">
+        <div className=" p-2 z-10 flex gap-4">
                     <button 
+                    ref={el=>tabRefs.current[0] = el}
                     className={`
-                    ${preview ? '  border-b-2 border-slate-300' : ' text-slate-400'}
-                    ${theme === 'light' ? 'text-black border-slate-800':'text-slate-200'}
+                    ${preview ? '  ' : ' text-slate-400 dark:text-slate-400'}
+                    text-black   dark:text-white  transition-all  duration-200 ease-linear z-10
+                    
                     px-2 py-1  `}
                     onClick={()=>setPreview(true)}>Preview</button>
                     <button 
-                    className={`${!preview ? ' border-b-2 border-slate-300' : ' text-slate-400'}
-                    ${theme === 'light' ? 'text-black border-slate-800':'text-slate-200'}
+                    ref={el=>tabRefs.current[1] = el}
+                    className={`${!preview ? ' ' : ' text-slate-400 dark:text-slate-400'}
+                    text-black    dark:text-slate-200 transition-all duration-200 ease-linear z-10
                     px-2 py-1 `}
                     onClick={()=>setPreview(false)}>Code</button>
+        </div>
+        <div
+          className="absolute bottom-0 left-0    h-10 mb-2  bg-slate-500 transition-all duration-300 ease-out rounded-md"
+          style={backgroundStyle}
+        />
     </div>
 )
 }

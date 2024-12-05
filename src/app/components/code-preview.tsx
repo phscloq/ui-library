3ab05f26f08e9e6025dfee4f5b31e943b97752e7
Baran <phscloq@gmail.com>
2024-12-05 12:09:47 +0300

    Update code-preview.tsx
    Updated styling.

--- src/app/components/code-preview.tsx
+++ src/app/components/code-preview.tsx
@@ -1,16 +1,13 @@
 export default function CodePreview({children}:{children:React.ReactNode}){
     return (
-        <div className=" 
-        relative
-       ">   
-        
-            <pre className=" rounded-md bg-slate-400  overflow-x-auto h-[400px]">
-                <code className="text-black text-sm">
+        <div className="relative">  
+            <pre className=" rounded-b-md bg-slate-700 dark:bg-slate-300 p-3  overflow-y-auto h-[400px]">
+                <code className="text-white dark:text-black text-sm ">
                     {children}
                 </code>
             </pre>
             <button
-            className="bg-slate-300 text-black px-2 py-1 
+            className="bg-slate-300 dark:bg-slate-700 dark:text-white text-black px-2 py-1 
             rounded-md absolute top-2 right-9 " 
             onClick={() => navigator.clipboard.writeText(children?.toString() || '')}>
                 Copy

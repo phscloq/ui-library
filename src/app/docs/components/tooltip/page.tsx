2f94b4d3b519c8f8bfc47b261e44525b2043df69
Baran <phscloq@gmail.com>
2024-12-08 23:36:25 +0300

    Update Tooltip Page
    Updated the button styling and the text prop.

--- src/app/docs/components/tooltip/page.tsx
+++ src/app/docs/components/tooltip/page.tsx
@@ -24,8 +24,10 @@ const [preview, setPreview] = useState(true)
             <div>
                 <TabSwitch preview={preview} setPreview={setPreview} />
                 
-                {preview ? <TooltipComponent text="Tooltip">
-                    <button>Hover over me</button>
+                {preview ? <TooltipComponent text="Copy the code">
+                    <button className="bg-slate-200 dark:bg-slate-600 dark:text-white text-black px-3 py-1 rounded-md 
+            hover:bg-slate-300  active:bg-slate-500 active:text-slate-100 dark:hover:bg-slate-700">
+                    Hover</button>
                 </TooltipComponent>:  <CodePreview>{code}</CodePreview>}
             </div>
             

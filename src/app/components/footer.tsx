70f21a83d833bec2364911e1ade88087118458ae
Baran <phscloq@gmail.com>
2025-01-02 22:29:21 +0300

    Create footer.tsx

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Footer(){
+    return (
+        <footer className="bg-gray-100 dark:bg-slate-800 py-8">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                
+
+                    <p className="text-center text-sm text-gray-600 dark:text-slate-300 ">© 2025 Baran UI. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}

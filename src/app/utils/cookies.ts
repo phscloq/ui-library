16d452e7481c35521e55864b7476598feda48998
Baran <phscloq@gmail.com>
2025-03-20 23:15:37 +0300

    Add `dismissCookieWarning` function to `utils/cookies.ts`
    - Added `dismissCookieWarning` function to set a cookie indicating the cookie warning has been dismissed.
    - Set the cookie to expire in 1 year and be available across the entire site.

--- src/app/utils/cookies.ts
+++ src/app/utils/cookies.ts
@@ -8,3 +8,10 @@ export async function create(theme:string) {
 export const getThemeCookie = async () => {
     return cookies().get('theme')?.value;
   };
+
+export async function dismissCookieWarning() {
+  cookies().set('cookieWarningDismissed', 'true', {
+    expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000), // Expires in 1 year
+    path: '/', // Available across the entire site
+  });
+}

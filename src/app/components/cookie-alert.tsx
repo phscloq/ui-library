f8c36fdd50aa379fcc159dfea77d6bebb581e8a6
Baran <phscloq@gmail.com>
2025-03-20 23:13:48 +0300

    Enhance `CookieAlert` component
    - Added `dismissCookieWarning` utility to handle cookie dismissal.
    - Updated `useEffect` to check for the dismissal state in cookies.
    - Implemented `handleDismiss` to save the dismissal state to cookies.
    - Updated the component to hide the alert based on the dismissal state from cookies.

--- src/app/components/cookie-alert.tsx
+++ src/app/components/cookie-alert.tsx
@@ -1,11 +1,21 @@
 'use client'
 import { useEffect, useState } from "react"
+import { dismissCookieWarning } from "../utils/cookies";
 export default function CookieAlert (){
     const [dismiss, setDismiss] = useState<Boolean>();
 
-    /* const handleDismiss = ()=>{
-        localStorage.setItem('cookie-dismiss', 'true')
-    } */
+    useEffect(() => {
+        const isDismissed = document.cookie.includes('cookieWarningDismissed=true');
+        if (isDismissed) {
+            setDismiss(true);
+        }
+    }, []);
+    
+    // Save dismissal state to cookies when the user clicks "Got it!"
+    const handleDismiss = async () => {
+    setDismiss(true);
+    await dismissCookieWarning();
+    };
 
     return(
         <div className={`fixed bottom-4 left-4  px-4 max-w-lg  flex
@@ -15,18 +25,19 @@ export default function CookieAlert (){
                 <div>
                     <h1 className=" text-3xl sm:text-5xl  font-medium
                     text-gray-800 dark:text-black mr-2">
-                      🍪 
+                    🍪 
                     </h1>
                 </div>
                 <div className="flex flex-col ">
                     <p className=" text-xs sm:text-base text-gray-600 dark:text-slate-300">
-                    We use cookies to remember your theme preference (dark/light mode). By continuing, you accept this use.
-                    <button className="underline text-black   hover:font-medum sm:text-base sm:ml-2 w-fit text-xs"
-                    onClick={()=>setDismiss(true)}
-                    >Got it!</button></p>
-                    
+                        We use cookies to remember your theme preference (dark/light mode). By continuing, you accept this use.
+                        <button className="underline text-black   hover:font-medum sm:text-base sm:ml-2 w-fit text-xs"
+                            onClick={handleDismiss}
+                        >
+                            Got it!
+                        </button>
+                    </p>
                 </div>
-               
             </div>
         </div>
     
